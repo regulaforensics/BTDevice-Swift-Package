6ab954da2bc1972037af70e4c37e12543bebac4f
Jenkins <jenkins@regula.by>
2026-09-04 11:32:57 +0300

    RC 9.8.670

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["BTDeviceStage"]),
     ],
     targets: [
-        .binaryTarget(name: "BTDeviceStage", url: "https://pods.regulaforensics.com/Stage/BTDeviceStage/9.8.669/BTDeviceStage-9.8.669.zip", checksum: "e38f8eccc042d82465bf68b7880fd589da706d53e001e47115c969709eaf7e47"),
+        .binaryTarget(name: "BTDeviceStage", url: "https://pods.regulaforensics.com/Stage/BTDeviceStage/9.8.670/BTDeviceStage-9.8.670.zip", checksum: "d0fc9c2ea1967c263c8a70e13018118b69e927603f25a83fb29b42d2e4fd152e"),
     ]
 )
