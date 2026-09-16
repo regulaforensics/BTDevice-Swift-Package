476a8de2c51779a6960211ac96f623e03259837e
Jenkins <jenkins@regula.by>
2026-09-16 03:22:36 +0300

    Nightly 9.8.698

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["BTDeviceNightly"]),
     ],
     targets: [
-        .binaryTarget(name: "BTDeviceNightly", url: "https://pods.regulaforensics.com/Nightly/BTDeviceNightly/9.9.697/BTDeviceNightly-9.9.697.zip", checksum: "69af5753769d60496372a9da81efef41f798eff265ae090684a781115f6bead1"),
+        .binaryTarget(name: "BTDeviceNightly", url: "https://pods.regulaforensics.com/Nightly/BTDeviceNightly/9.8.698/BTDeviceNightly-9.8.698.zip", checksum: "65385979b452982651c3d0715585e45dd3477c4082524a3ac23df87f9fad6ae2"),
     ]
 )
